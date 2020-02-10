0cadd970570e5ced8048b5c481ae40c47bb2484d
hector galicia milla <hectormilla8@gmail.com>
2020-02-10 15:59:56 -0500

    created 3 questions using constructor

--- codingchallenge.js
+++ codingchallenge.js
@@ -5,4 +5,12 @@ function Question (question, answerChoices, correctAnswer) {
      this.correctAnswer = correctAnswer
 }
 
+let question1 = new Question('what is the best console',
+ ['xbox', 'ps4', 'switch', 'all the above'], '4')
+
+let question2 = new Question('what is the best food',
+ ['pizza', 'tacos', 'pasta', 'chicken'], '1')
+
+let question2 = new Question('what is the best season', 
+['winter', 'summer', 'fall', 'spring'], '4')
 
