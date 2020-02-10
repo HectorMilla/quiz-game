eaa612109768ce1f0ace99bc15aa6f8eb91c43a6
hector galicia milla <hectormilla8@gmail.com>
2020-02-10 15:50:35 -0500

    created question constructor function

---
+++ codingchallenge.js
@@ -0,0 +1,8 @@
+//quiz game
+function Question (question, answerChoices, correctAnswer) {
+     this.question = question,
+     this.answerChoices = answerChoices,
+     this.correctAnswer = correctAnswer
+}
+
+
